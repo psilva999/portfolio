9f0dfdd54d27fe9f96d5cda67016de9eae23b35a
PSILVA999 <p.silva0399@gmail.com>
2023-07-25 09:10:21 -0300

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,4 @@
-import { ReactComponent as Github } from '../assets/svg/4-github.svg'
-import { ReactComponent as Telegram } from '../assets/svg/5-telegram.svg'
-import { ReactComponent as Linkedin } from '../assets/svg/6-linkedin.svg'
+import Network from "./Network"
 
 const Footer = () => {
   return (
@@ -11,16 +9,7 @@ const Footer = () => {
         <p>Take whats left and live it properly</p>
       </article>
 
-      <div>
-        <a href="https://github.com/psilva999" rel='noreferrer' target='_blank'>
-          <Github/> </a>
-
-        <a href="https://t.me/psilva999" rel='noreferrer' target='_blank'>
-          <Telegram/> </a>
-
-        <a href="https://www.linkedin.com/in/psilva999/" rel='noreferrer' target='_blank'>
-          <Linkedin/> </a>
-      </div>
+      <Network/>
     </footer>
   )
 }
---
+++ src/components/Network.jsx
@@ -0,0 +1,20 @@
+import { ReactComponent as Github } from '../assets/svg/4-github.svg'
+import { ReactComponent as Telegram } from '../assets/svg/5-telegram.svg'
+import { ReactComponent as Linkedin } from '../assets/svg/6-linkedin.svg'
+
+const Network = () => {
+  return (
+    <div>
+      <a href="https://github.com/psilva999" rel='noreferrer' target='_blank'>
+        <Github/> </a>
+
+      <a href="https://t.me/psilva999" rel='noreferrer' target='_blank'>
+        <Telegram/> </a>
+
+      <a href="https://www.linkedin.com/in/psilva999/" rel='noreferrer' target='_blank'>
+        <Linkedin/> </a>
+    </div>
+  )
+}
+
+export default Network
